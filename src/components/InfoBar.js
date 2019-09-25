c320aff9a235b9a3dd0f8397e1f23f233212afbe
Andy Bettisworth <andy@accreu.com>
2019-09-24 20:19:29 -0500

    Add audio controls

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import Row from '../styles/Row';
+import Column from '../styles/Column';
 import Heart from '../assets/images/interface/health.png';
+import PlayBtn from '../assets/images/interface/play-btn.png';
+import PauseBtn from '../assets/images/interface/pause-btn.png';
 
-function InfoBar() {
+function InfoBar(props) {
   const [health, setHealth] = useState(5)
   let healthIndicator = []
 
@@ -10,9 +14,17 @@ function InfoBar() {
   }
 
   return (
-    <div>
-      {healthIndicator}
-    </div>
+    <Row>
+      <Column width={10}>
+        {healthIndicator}
+      </Column>
+
+      <Column width={2} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <button onClick={props.toggleMusic} style={{ backgroundColor: 'inherit', border: 'inherit', outline: 'none' }}>
+          <img src={props.isPlaying ? PauseBtn : PlayBtn} width="60px" height="60px" alt="Play Button" />
+        </button>
+      </Column>
+    </Row>
   )
 }
 
--- src/views/Game.js
+++ src/views/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import InfoBar from '../components/InfoBar';
 import Map from '../components/Map';
 import Feed from '../components/Feed';
@@ -8,29 +8,46 @@ import Theme from '../styles/Theme';
 import Row from '../styles/Row';
 import Column from '../styles/Column';
 import NavBar from '../components/NavBar';
+import Arrival from '../assets/audio/arrival-audio.mp3';
 
 const Game = () => {
+  const [isPlaying, setIsPlaying] = useState(false)
+  const audioRef = useRef(null)
+
+  function toggleMusic() {
+    if (audioRef.current.paused) {
+      audioRef.current.play()
+      setIsPlaying(true)
+    } else {
+      audioRef.current.pause()
+      setIsPlaying(false)
+    }
+  }
+
   return (
     <>
-    <NavBar/>
-    <Theme>
-      <Container>
-        <Row>
-          <Column><InfoBar /></Column>
-        </Row>
+      <NavBar/>
 
-        <Row>
-          <Column>
-            <Map />
-          </Column>
-        </Row>
+      <Theme>
+        <Container>
+          <Row>
+            <Column><InfoBar toggleMusic={toggleMusic} isPlaying={isPlaying} /></Column>
+          </Row>
 
-        <Row>
-          <Column width={6}><Feed /></Column>
-          <Column width={6}><Controls /></Column>
-        </Row>
-      </Container>
-    </Theme>
+          <Row>
+            <Column>
+              <Map />
+            </Column>
+          </Row>
+
+          <Row>
+            <Column width={6}><Feed /></Column>
+            <Column width={6}><Controls /></Column>
+          </Row>
+        </Container>
+      </Theme>
+
+      <audio ref={audioRef} src={Arrival} loop={true} />
     </>
   )
 }
