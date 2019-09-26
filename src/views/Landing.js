e8d6414ba852fc11f27620afc71ca7a70bcb5647
Rogelio <rogercp1234@yahoo.com>
2019-09-26 11:00:30 -0700

    fixed explore button

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -2,23 +2,21 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import Column from '../styles/Column';
 
 const Landing = () => {
   return (
     <>
       <div className="body">
 
-      <div className="leftHalf" ></div>
+      {/* <div className="leftHalf" ></div>
 
-      <div className="rightHalf"></div>
+      <div className="rightHalf"></div> */}
           
-          <div>
+          
             <NavBar />
 
-            <div style={{ display: "flex",
-            justifyContent: "center",
-              alignTtems: "center",
-              marginTop:"203px"}}>
+            <div style={{display:"flex",flexDirection:"column", alignItems:"center",justifyContent:"center",height:"80vh"}}>
              
               <Link className="go" to="/login">
                           EXPLORE
@@ -27,7 +25,7 @@ const Landing = () => {
             </div> 
 
           </div>
-      </div>
+     
 
 
 </>
